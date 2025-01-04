c86ec9853b712a52255907c92ca560fd773e59f3
Siavash Yasini <siavash.yasini@gmail.com>
2025-01-03 22:54:31 -0800

    add chat and model mappings

--- streamlitissues/mappings.py
+++ streamlitissues/mappings.py
@@ -30,3 +30,22 @@ sorting_mapping = {
     "Most Reactions First": ("reaction_total_count", False),
     "Most Recently Updated First": ("updated_at", False),
 }
+
+# mapping for the chat avatars
+avatar_mapping = {
+    "user": None,
+    "ai": "🎈"
+}
+    
+# mapping for the model token sizes
+model_token_sizes = {
+    "mistral-7b": 32000,
+    "mistral-large": 32000,
+    "mistral-large2": 128000,
+    "mixtral-8x7b": 32000,
+    "llama2-70b-chat": 4096,
+    "llama3.2-1b": 128000,
+    "llama3.2-3b": 128000,
+    "gemma-7b": 8000,
+    "snowflake-arctic": 4096,
+}
